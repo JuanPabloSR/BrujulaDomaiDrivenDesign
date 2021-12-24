4519d648a5e349dec0009b485cb4d9746089a61f
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2021-12-24 13:39:48 -0500

    Primer Commit

---
+++ src/test/java/co/com/sofka/Brujula/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.Brujula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
